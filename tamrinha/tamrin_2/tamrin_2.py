fbfed2115639f00c7f4ca564ed67b10540b57a20
MohammadMehdiSharifinia <369mmahdi369@gmail.com>
2025-02-25 12:17:10 +0330

    first commit

---
+++ tamrinha/tamrin_2/tamrin_2.py
@@ -0,0 +1,7 @@
+#این کد برای سال های میلادی استفادهمیشود
+year = int(input("Enter a year: "))
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(f"{year} is a leap year.")
+else:
+    print(f"{year} is not a leap year.")
