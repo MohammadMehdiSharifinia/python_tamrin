bbdd7b188d04e14904f0e565038077d3fb8d8538
MohammadMehdiSharifinia <369mmahdi369@gmail.com>
2025-04-26 21:05:55 +0330

    Finish writing the code

--- Quiz/Third.py
+++ Quiz/Third.py
@@ -2,7 +2,7 @@ import random
 from itertools import combinations
 
 nums = [random.randint(1, 15) for _ in range(20)]
-print(nums)
+
 
 for r in range(2, len(nums) + 1):
     for combo in combinations(nums, r):
