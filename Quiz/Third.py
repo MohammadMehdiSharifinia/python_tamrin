f830b28bafe5b0a7d584aa045ae1f4e1c47c312b
MohammadMehdiSharifinia <369mmahdi369@gmail.com>
2025-04-26 21:23:29 +0330

    Finish writing the code

--- Quiz/Third.py
+++ Quiz/Third.py
@@ -1,13 +1,17 @@
 import random
-from itertools import combinations
 
 nums = [random.randint(1, 15) for _ in range(20)]
 
+def d (arr, r):
+    if r == 0: return [[]]
+    if not arr: return []
+    return d(arr[1:], r) + [[arr[0]] + x for x in d (arr[1:], r-1)]
 
-for r in range(2, len(nums) + 1):
-    for combo in combinations(nums, r):
-        if 30 <= sum(combo) <= 32:
-            print(combo)
-print("*****************************")            
+for r in range(2, len(nums)+1):
+    for c in d (nums, r):
+        if 30 <= sum(c) <= 32:
+            print(c)
+
+print("*********")
 print(nums)
-print("*****************************") 
+print("*********")
